67540339ee1f3fdd8a126e1d7f2745f6ae8afce4
Borna Goljacki <borna.goljacki@gmail.com>
2021-01-10 17:46:04 +0100

    Add HW1

---
+++ Homework_1/Matrix/Matrix.cs
@@ -0,0 +1,477 @@
+﻿using System;
+
+namespace Homework_1
+{
+    public class Matrix
+    {
+        public double[,] Elements { get; }
+        public int? Permutations { get; set; }
+
+        private double this[int i, int j]
+        {
+            get => Elements[i, j];
+            set => Elements[i, j] = value;
+        }
+        
+        /// <summary>
+        /// Creates a Matrix from an existing <see cref="array"/> of elements.
+        /// </summary>
+        /// <param name="array"></param>
+        public Matrix(double[,] array)
+        {
+            Elements = array;
+
+            for (var i = 0; i < Elements.GetLength(0); i++)
+            for (var j = 0; j < Elements.GetLength(1); j++)
+                Elements[i, j] = array[i, j];
+        }
+
+        /// <summary>
+        /// Creates a square Matrix with all elements initialised to 0.
+        /// </summary>
+        /// <param name="n"></param>
+        public Matrix(int n) => Elements = new double[n, n];
+
+        /// <summary>
+        /// Creates a Matrix with varying rows and columns where all elements are initialised to <see cref="fill"/>.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="fill"></param>
+        public Matrix(double[,] array, double fill = 0)
+        {
+            Elements = array;
+
+            for (var i = 0; i < Elements.GetLength(0); i++)
+            for (var j = 0; j < Elements.GetLength(1); j++)
+                Elements[i, j] = fill;
+        }
+
+        /// <summary>
+        /// Decomposes the A matrix into PA = LU.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns>Permutation Matrix containing the number of permutations as well.</returns>
+        public static Matrix LUPDecomposition(Matrix A)
+        {
+            var rows = A.Elements.GetLength(0);
+            var cols = A.Elements.GetLength(1);
+            if (rows != cols) return null;
+
+            var P = IdentityMatrix(rows);
+            var permutations = 0;
+            for (var i = 0; i < rows - 1; i++)
+            {
+                var pivot = i;
+                for (var j = i + 1; j < cols; j++)
+                    if (Math.Abs(A[j, i]) > Math.Abs(A[pivot, i]))
+                        pivot = j;
+
+                A = Swap(A, i, pivot);
+                P = Swap(P, i, pivot);
+                permutations++;
+
+                // if (Math.Abs(A[pivot, i]) < (double)0.00000000001)
+                // {
+                //     Console.WriteLine("Pivot is nearing 0!");
+                //     return null;
+                // }
+                for (var j = i + 1; j < rows; j++)
+                {
+                    A[j, i] /= A[i, i];
+                    for (var k = i + 1; k < cols; k++)
+                        A[j, k] -= A[j, i] * A[i, k];
+                }
+            }
+
+            P.Permutations = permutations;
+            return P;
+        }
+
+        /// <summary>
+        /// Calculates the determinant of the given matrix A.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static double Determinant(Matrix A)
+        {
+            var n = A.Elements.GetLength(0);
+            var P = LUPDecomposition(A);
+
+            var result = 1.0;
+            
+            if (P.Permutations != null) 
+                result *= Math.Pow(-1, P.Permutations.Value);
+
+            var U = UpperTriangularMatrix(A);
+            
+            for (var i = 0; i < n; i++) 
+                result *= U[i, i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// Creates an I (identity) matrix with the dimensions of n x n.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static Matrix IdentityMatrix(int n)
+        {
+            var identity = new Matrix(new double[n, n]);
+            for (var i = 0; i < n; i++)
+                identity[i, i] = 1;
+
+            return identity;
+        }
+
+
+        /// <summary>
+        /// Swaps the i-th and j-th row of the matrix a.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private static Matrix Swap(Matrix a, int i, int j)
+        {
+            for (var k = 0; k < a.Elements.GetLength(0); k++)
+            {
+                var temp = a[i, k];
+                a[i, k] = a[j, k];
+                a[j, k] = temp;
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Decomposes the given matrix A into A = LU;
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static Matrix LUDecomposition(Matrix A)
+        {
+            var rows = A.Elements.GetLength(0);
+
+            for (var i = 0; i < rows - 1; i++)
+            {
+                if (Math.Abs(A[i, i]) < 0.00000000001)
+                {
+                    Console.WriteLine("Pivot is nearing 0!");
+                    A[i, i] = Math.Sign(A[i, i]) * 0.0001;
+                }
+                for (var j = i + 1; j < rows; j++)
+                {
+                    A[j, i] /= A[i, i];
+                    for (var k = i + 1; k < rows; k++)
+                        A[j, k] -= A[j, i] * A[i, k];
+                }
+            }
+            return A;
+        }
+
+        /// <summary>
+        /// Calculates the inverse of a given matrix A.
+        /// A * A^(-1) = I
+        /// The inverse can only be calculated when the determinant is not 0.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static Matrix Inverse(Matrix A)
+        {
+            var n = A.Elements.GetLength(0);
+
+            var detA = Determinant(A);
+
+            if (detA < 0.00001) return null;
+            
+            var vector = new Matrix(new double[n, 1]);
+
+            var permutationMatrix = LUPDecomposition(A);
+            
+            var result = new Matrix(n);
+            for (var i = 0; i < n; i++) {
+                for (var j = 0; j < n; j++) 
+                    vector[j, 0] = permutationMatrix[j, i];
+                
+                var fwd =  ForwardSubstitution(A, vector);
+                var bck = BackwardSubstitution(A, fwd);
+                for (var j = 0; j < n; j++) 
+                    result[j, i] = bck[j, 0];
+            }
+            return result;
+        }
+        
+        /// <summary>
+        /// Completes the forward substitution of the given matrix A and vector.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public static Matrix ForwardSubstitution(Matrix A, Matrix vector)
+        {
+            // "vector" and "B" and both vectors, with only a singular row.
+            // var B = vector;
+            var aRows = A.Elements.GetLength(0);
+            var aCols = A.Elements.GetLength(1);
+
+            var vectorRows = vector.Elements.GetLength(0);
+
+            if (aRows != vectorRows) return null;
+
+            for (var i = 0; i < aRows - 1; i++)
+            for (var j = i + 1; j < aCols; j++)
+                vector[j, 0] -= A[j, i] * vector[i, 0];
+
+            return vector;
+        }
+        
+        /// <summary>
+        /// Completes the backward substitution of the given matrix A and vector.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public static Matrix BackwardSubstitution(Matrix A, Matrix vector)
+        {
+            // "vector" and "B" and both vectors, with only a singular row.
+            var rows = A.Elements.GetLength(0);
+            var cols = A.Elements.GetLength(1);
+            
+            var vectorRows = vector.Elements.GetLength(0);
+
+            if (rows != vectorRows) return null;
+            
+            for (var i = rows - 1; i >= 0; i--)
+            {
+                vector[i, 0] /= A[i, i];
+                for (var j = 0; j < i; j++)
+                    vector[j, 0] -= A[j, i] * vector[i, 0];
+            }
+            return vector;
+        }
+
+        /// <summary>
+        /// Adds the elements of the 2 given matrices together.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var bRows = b.Elements.GetLength(0);
+
+            var aCols = a.Elements.GetLength(1);
+            var bCols = b.Elements.GetLength(1);
+
+            if (aRows != bRows || aCols != bCols) return null;
+            
+            var result = new Matrix(new double[aRows, aCols]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+                result[i, j] = a[i, j] + b[i, j];
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Subtracts the elements of the matrix a with the elements of matrix b and returns a new matrix containing
+        /// the result of the operation.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var bRows = b.Elements.GetLength(0);
+
+            var aCols = a.Elements.GetLength(1);
+            var bCols = b.Elements.GetLength(1);
+
+            if (aRows != bRows || aCols != bCols) return null;
+            
+            var result = new Matrix(new double[aRows, aCols]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+                result[i, j] = a[i, j] - b[i, j];
+
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplies the matrix with the scalar value.
+        /// </summary>
+        /// <param name="scalar"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Matrix operator *(double scalar, Matrix a) => a * scalar;
+        
+        /// <summary>
+        /// Multiplies the matrix with the scalar value.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
+        public static Matrix operator *(Matrix a, double scalar)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var aCols = a.Elements.GetLength(1);
+            
+            var result = new Matrix(new double[aRows, aCols]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+                result[i, j] = a[i, j] * scalar;
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Multiplies the 2 given matrices together.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var bRows = b.Elements.GetLength(0);
+
+            var aCols = a.Elements.GetLength(1);
+            var bCols = b.Elements.GetLength(1);
+
+            if (aCols != bRows) return null;
+
+            var result = new Matrix(new double[aRows, bCols]);
+
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < bCols; j++)
+            {
+                double tmp = 0;
+                for (var k = 0; k < aCols; k++)
+                {
+                    tmp += a[i, k] * b[k, j];
+                }
+                result[i, j] = tmp;
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Transposes the given matrix A.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static Matrix Transpose(Matrix A)
+        {
+            var aRows = A.Elements.GetLength(0);
+            var aCols = A.Elements.GetLength(1);
+            var result = new Matrix(new double[aCols, aRows]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+                result[j, i] = A[i, j];
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the lower triangular matrix of the given matrix.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Matrix LowerTriangularMatrix(Matrix a)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var aCols = a.Elements.GetLength(1);
+            var result = new Matrix(new double[aRows, aCols]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+            {
+                if (i < j) result[i, j] = 0.0;
+                else if (i == j) result[i, j] = 1.0;
+                else result[i, j] = a[i, j];
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Returns the upper triangular matrix of the given matrix.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Matrix UpperTriangularMatrix(Matrix a)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var aCols = a.Elements.GetLength(1);
+            var result = new Matrix(new double[aRows, aCols]);
+            
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+            {
+                if (i > j) result[i, j] = 0.0;
+                else result[i, j] = a[i, j];
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Returns true if the 2 given matrices are equal in size and elements.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            var aRows = a.Elements.GetLength(0);
+            var bRows = b.Elements.GetLength(0);
+
+            var aCols = a.Elements.GetLength(1);
+            var bCols = b.Elements.GetLength(1);
+
+            if (aRows != bRows || aCols != bCols) return false;
+
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < aCols; j++)
+                if (Math.Abs(a[i, j] - b[i, j]) > 0.00000000001)
+                    return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Returns true if the 2 given matrices are not equal either in size or elements.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(Matrix a, Matrix b) => !(a == b);
+
+        /// <summary>
+        /// Overrides the default ToString in order to better visualise the matrix.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var result = "___________________________________\n";
+            for (var i = 0; i < Elements.GetLength(0); i++)
+            {
+                result += "|";
+                for (var j = 0; j < Elements.GetLength(1); j++)
+                    result += $"      {Elements[i, j]:0.00000}";
+
+                result += "      |\n";
+            }
+            result += "___________________________________";
+            return result;
+        }
+        
+        
+    }
+}
---
+++ Homework_1/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace Homework_1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine("Zadatak 2 LUP:");
+            var zadatak2 =  ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak2.txt");
+            var vector2 = new Matrix(new double[3, 1] {{12}, {12}, {1}});
+            var permutationMatrix2 = Matrix.LUPDecomposition(zadatak2);
+            var permutationVector2 = permutationMatrix2 * vector2;
+            var fwd2 = Matrix.ForwardSubstitution(zadatak2, permutationVector2);
+            var bck2 = Matrix.BackwardSubstitution(zadatak2, fwd2);
+            var solution2 = bck2;
+            Console.WriteLine(solution2);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            
+            Console.WriteLine("Zadatak 2 LU:");
+            var zadatak2Lu =  ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak2.txt");
+            var vector2Lu = new Matrix(new double[3, 1] {{12}, {12}, {1}});
+            var permutationMatrix2Lu = Matrix.LUPDecomposition(zadatak2Lu);
+            var permutationVector2Lu = permutationMatrix2Lu * vector2Lu;
+            var fwd2Lu = Matrix.ForwardSubstitution(zadatak2Lu, permutationVector2Lu);
+            var bck2Lu = Matrix.BackwardSubstitution(zadatak2Lu, fwd2Lu);
+            var solution2Lu = bck2Lu;
+            Console.WriteLine(solution2Lu);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+
+            Console.WriteLine("Zadatak 3 LUP:");
+            var zadatak3 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak3.txt");
+            var vector3 = new Matrix(new double[3, 1] {{4}, {2}, {3}});
+            var permutationMatrix3 = Matrix.LUPDecomposition(zadatak3);
+            var permutationVector3 = permutationMatrix3 * vector3;
+            var fwd3 = Matrix.ForwardSubstitution(zadatak3, permutationVector3);
+            var bck3 = Matrix.BackwardSubstitution(zadatak3, fwd3);
+            var solution3 = bck3;
+            Console.WriteLine(solution3);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            
+            Console.WriteLine("Zadatak 3 LU:");
+            var zadatak3Lu = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak3.txt");
+            var vector3Lu = new Matrix(new double[3, 1] {{4}, {2}, {3}});
+            var permutationMatrix3Lu = Matrix.LUPDecomposition(zadatak3Lu);
+            var permutationVector3Lu = permutationMatrix3Lu * vector3Lu;
+            var fwd3Lu = Matrix.ForwardSubstitution(zadatak3Lu, permutationVector3Lu);
+            var bck3Lu = Matrix.BackwardSubstitution(zadatak3Lu, fwd3Lu);
+            var solution3Lu = bck3Lu;
+            Console.WriteLine(solution3Lu);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 4 LUP:");
+            var zadatak4 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak4.txt");
+            var vector4 = new Matrix(new double[3, 1] {{12000000.000001}, {14000000}, {10000000}});
+            var permutationMatrix4 = Matrix.LUPDecomposition(zadatak4);
+            var permutationVector4 = permutationMatrix4 * vector4;
+            var fwd4 = Matrix.ForwardSubstitution(zadatak4, permutationVector4);
+            var bck4 = Matrix.BackwardSubstitution(zadatak4, fwd4);
+            var solution4 = bck4;
+            Console.WriteLine(solution4);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            
+            Console.WriteLine("Zadatak 4 LU:");
+            var zadatak4Lu = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak4.txt");
+            var vector4Lu = new Matrix(new double[3, 1] {{12000000.000001}, {14000000}, {10000000}});
+            var permutationMatrix4Lu = Matrix.LUPDecomposition(zadatak4Lu);
+            var permutationVector4Lu = permutationMatrix4Lu * vector4Lu;
+            var fwd4Lu = Matrix.ForwardSubstitution(zadatak4Lu, permutationVector4Lu);
+            var bck4Lu = Matrix.BackwardSubstitution(zadatak4Lu, fwd4Lu);
+            var solution4Lu = bck4Lu;
+            Console.WriteLine(solution4Lu);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 5 LUP:");
+            var zadatak5 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak5.txt");
+            var vector5 = new Matrix(new double[3, 1] {{6}, {9}, {3}});
+            var permutationMatrix5 = Matrix.LUPDecomposition(zadatak5);
+            var permutationVector5 = permutationMatrix5 * vector5;
+            var fwd5 = Matrix.ForwardSubstitution(zadatak5, permutationVector5);
+            var bck5 = Matrix.BackwardSubstitution(zadatak5, fwd5);
+            var solution5 = bck5;
+            Console.WriteLine(solution5);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            
+            Console.WriteLine("Zadatak 5 LU:");
+            var zadatak5Lu = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak5.txt");
+            var vector5Lu = new Matrix(new double[3, 1] {{6}, {9}, {3}});
+            var permutationMatrix5Lu = Matrix.LUPDecomposition(zadatak5Lu);
+            var permutationVector5Lu = permutationMatrix5Lu * vector5Lu;
+            var fwd5Lu = Matrix.ForwardSubstitution(zadatak5Lu, permutationVector5Lu);
+            var bck5Lu = Matrix.BackwardSubstitution(zadatak5Lu, fwd5Lu);
+            var solution5Lu = bck5Lu;
+            Console.WriteLine(solution5Lu);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 6 LUP:");
+            var zadatak6 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak6.txt");
+            var vector6 = new Matrix(new double[3, 1] {{9000000000}, {15}, {0.0000000015}});
+            var permutationMatrix6 = Matrix.LUPDecomposition(zadatak6);
+            var permutationVector6 = permutationMatrix6 * vector6;
+            var fwd6 = Matrix.ForwardSubstitution(zadatak6, permutationVector6);
+            var bck6 = Matrix.BackwardSubstitution(zadatak6, fwd6);
+            var solution6 = bck6;
+            Console.WriteLine(solution6);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            
+            Console.WriteLine("Zadatak 6 LU:");
+            var zadatak6Lu = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak6.txt");
+            var vector6Lu = new Matrix(new double[3, 1] {{9000000000}, {15}, {0.0000000015}});
+            var permutationMatrix6Lu = Matrix.LUDecomposition(zadatak6Lu);
+            var permutationVector6Lu = permutationMatrix6Lu * vector6Lu;
+            var fwd6Lu = Matrix.ForwardSubstitution(zadatak6Lu, permutationVector6Lu);
+            var bck6Lu = Matrix.BackwardSubstitution(zadatak6Lu, fwd6Lu);
+            var solution6Lu = bck6Lu;
+            Console.WriteLine(solution6Lu);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 7:");
+            var zadatak7 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak7.txt");
+            var zad7Inverse = Matrix.Inverse(zadatak7);
+            var result = zad7Inverse?.ToString();
+            result = string.IsNullOrEmpty(result)
+                ? "The result was null because the determinant is 0."
+                : "The result wasn't null.";
+            Console.WriteLine(result);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 8:");
+            var zadatak8 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak8.txt");
+            var zad8Inverse = Matrix.Inverse(zadatak8);
+            Console.WriteLine(zad8Inverse);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 9:");
+            var zadatak9 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak9.txt");
+            var zad9Det = Matrix.Determinant(zadatak9);
+            Console.WriteLine(zad9Det);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            Console.WriteLine("Zadatak 10:");
+            var zadatak10 = ReadMatrixFromFile(@"C:\Faks\APR\Homework_1\Files\Zadatak10.txt");
+            var zad10Det = Matrix.Determinant(zadatak10);
+            Console.WriteLine(zad10Det);
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+        }
+
+        private static void PrintMatrixToFile(string path, Matrix matrix, string fileName)
+        {
+            using var sw = new StreamWriter($"{path}\\{fileName}.txt");
+            
+            
+            for (var i = 0; i < matrix.Elements.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix.Elements.GetLength(1); j++)
+                {
+                    sw.Write($" {matrix.Elements[i, j]}");
+                }
+                sw.Write("\n");
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+        private static Matrix ReadMatrixFromFile(string path)
+        {
+            var rows = File.ReadAllLines(path);
+            var cols = rows[0].Trim().Split(' ');
+            var result = new double[rows.Length, cols.Length];
+            
+            var i = 0;
+            foreach (var row in rows)
+            {
+                var j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    double.TryParse(col.Trim(), out result[i, j]);
+                    j++;
+                }
+                i++;
+            }
+            return new Matrix(result);
+        }
+    }
+}
