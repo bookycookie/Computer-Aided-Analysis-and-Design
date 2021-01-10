096bee7ae2d73a7a7d18e111746c18ed272c2586
Borna Goljacki <borna.goljacki@gmail.com>
2021-01-10 01:24:59 +0100

    Finish up writing assignments. TODO: PECE and P(EC)^2E

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -11,124 +11,172 @@ namespace Homework_5
 
         static void Main(string[] args)
         {
-            Assignment1();
+            // Assignment1(verbose: false);
+            // Assignment2(verbose: false);
+            // Assignment3(verbose: false);
+            Assignment4(verbose: false);
         }
 
-        private static void Assignment1()
+        private static void Assignment1(double T = 0.01, int tmax = 10, bool verbose = true)
         {
-            const double T = 0.01;
-            const int tmax = 10;
-            const double t = 0.0;
-
+            Console.WriteLine("ASSIGNEMENT 1");
             var start = VectorBuilder.Dense(new[] {1.0, 1.0});
             var A = MatrixBuilder.Dense(2, 2, new[] {0.0, -1.0, 1.0, 0.0});
             var B = MatrixBuilder.Dense(2, 2);
             Func<double, double> rt = _ => 0.0;
 
-            // var eulerError = Euler(t, tmax, T, start, A, B, rt, verbose: true);
-            // var reverseEulerError = ReverseEuler(t, tmax, T, start, A, B, rt, verbose: true);
-            // var trapezoidError = Trapezoid(t, tmax, T, start, A, B, rt, verbose: true);
-            // var rungeKuttaError = RungeKutta(t, tmax, T, start, A, B, rt, verbose: true);
+            var euler = Euler(tmax, T, start, A, B, rt, verbose);
+            var reverseEuler = ReverseEuler(tmax, T, start, A, B, rt, verbose);
+            var trapezoid = Trapezoid(tmax, T, start, A, B, rt, verbose);
+            var rungeKutta = RungeKutta(tmax, T, start, A, B, rt, verbose);
         }
 
-        private static double Euler(double time, double tmax, double T, Vector<double> start, Matrix<double> A,
+        private static void Assignment2(double T = 0.1, int tmax = 1, bool verbose = true)
+        {
+            Console.WriteLine("ASSIGNMENT 2");
+            var start = VectorBuilder.Dense(new[] {1.0, -2.0});
+            var A = MatrixBuilder.Dense(2, 2, new[] {0.0, -200.0, 1.0, -102.0});
+            var B = MatrixBuilder.Dense(2, 2);
+            Func<double, double> rt = _ => 0.0;
+
+            var euler = Euler(tmax, T, start, A, B, rt, verbose);
+            var reverseEuler = ReverseEuler(tmax, T, start, A, B, rt, verbose);
+            var trapezoid = Trapezoid(tmax, T, start, A, B, rt, verbose);
+            var rungeKutta = RungeKutta(tmax, T, start, A, B, rt, verbose);
+        }
+
+        private static void Assignment3(double T = 0.01, int tmax = 10, bool verbose = true)
+        {
+            Console.WriteLine("ASSIGNMENT 3");
+            var start = VectorBuilder.Dense(new[] {1.0, 3.0});
+            var A = MatrixBuilder.Dense(2, 2, new[] {0.0, 1.0, -2.0, -3.0});
+            var B = MatrixBuilder.Dense(2, 2, new[] {2.0, 0.0, 0.0, 3.0});
+            Func<double, double> rt = _ => 1.0;
+
+            var euler = Euler(tmax, T, start, A, B, rt, verbose);
+            var reverseEuler = ReverseEuler(tmax, T, start, A, B, rt, verbose);
+            var trapezoid = Trapezoid(tmax, T, start, A, B, rt, verbose);
+            var rungeKutta = RungeKutta(tmax, T, start, A, B, rt, verbose);
+        }
+
+        private static void Assignment4(double T = 0.01, int tmax = 1, bool verbose = true)
+        {
+            Console.WriteLine("ASSIGNMENT 4");
+            var start = VectorBuilder.Dense(new[] {-1.0, 3.0});
+            var A = MatrixBuilder.Dense(2, 2, new[] {1.0, 1.0, -5.0, -7.0});
+            var B = MatrixBuilder.Dense(2, 2, new[] {5.0, 0.0, 0.0, 3.0});
+            Func<double, double> rt = t => t;
+
+            var euler = Euler(tmax, T, start, A, B, rt, verbose);
+            var reverseEuler = ReverseEuler(tmax, T, start, A, B, rt, verbose);
+            var trapezoid = Trapezoid(tmax, T, start, A, B, rt, verbose);
+            var rungeKutta = RungeKutta(tmax, T, start, A, B, rt, verbose);
+        }
+
+        private static Vector<double> Euler(double tmax, double T, Vector<double> start, Matrix<double> A,
             Matrix<double> B, Func<double, double> r, bool verbose = false)
         {
+            Console.WriteLine($"\nEULER(tmax: {tmax}, T: {T}, start: [{string.Join(' ', start.ToArray())}])");
             var x = VectorBuilder.DenseOfVector(start);
-            var cumulativeError = 0.0;
+            var cumulativeError = VectorBuilder.Dense(A.RowCount);
             for (var t = T; t <= tmax; t += T)
             {
                 var pendulum = Generator.GeneratePendulum(start, t);
                 x += T * A * x;
 
-                cumulativeError += (x - pendulum).L2Norm();
+                cumulativeError += Vector<double>.Abs(x - pendulum);
 
                 if (verbose)
                     Console.WriteLine(
-                        $"t: {t:F2} -- x: [{x[0]:g4} {x[1]:g4}] -- pendulum: [{pendulum[0]:g4} {pendulum[1]:g4}]");
+                        $"t: {t:F2} -- x: [{string.Join(' ', x.ToArray())}] -- pendulum: {string.Join(' ', pendulum.ToArray())}]");
             }
 
-            if (verbose)
-                Console.WriteLine($"\nCumulative error: {cumulativeError}");
-            return cumulativeError;
+            Console.WriteLine($"Cumulative error: [{string.Join(' ', cumulativeError.ToArray())}]");
+
+            return x;
         }
 
-        private static double ReverseEuler(double time, double tmax, double T, Vector<double> start, Matrix<double> A,
+        private static Vector<double> ReverseEuler(double tmax, double T, Vector<double> start, Matrix<double> A,
             Matrix<double> B, Func<double, double> r, bool verbose = false)
         {
+            Console.WriteLine($"\nREVERSE EULER(tmax: {tmax}, T: {T}, start: [{string.Join(' ', start.ToArray())}])");
             var U = MatrixBuilder.DenseIdentity(A.ColumnCount);
             var P = (U - A * T).Inverse();
             var Q = P * T * B;
 
             var x = VectorBuilder.DenseOfVector(start);
-            var cumulativeError = 0.0;
+            var cumulativeError = VectorBuilder.Dense(A.RowCount);
             for (var t = T; t <= tmax; t += T)
             {
                 var pendulum = Generator.GeneratePendulum(start, t);
-                x = P * x + Q * VectorBuilder.Dense(new[] { r(t), r(t) });
-                cumulativeError += (x - pendulum).L2Norm();
+                x = P * x + Q * VectorBuilder.Dense(new[] {r(t), r(t)});
+                cumulativeError += Vector<double>.Abs(x - pendulum);
 
                 if (verbose)
                     Console.WriteLine(
-                        $"t: {t:F2} -- x: [{x[0]:g4} {x[1]:g4}] -- pendulum: [{pendulum[0]:g4} {pendulum[1]:g4}]");
+                        $"t: {t:F2} -- x: [{string.Join(' ', x.ToArray())}] -- pendulum: {string.Join(' ', pendulum.ToArray())}]");
             }
 
-            if (verbose)
-                Console.WriteLine($"\nCumulative error: {cumulativeError}");
-            return cumulativeError;
+            Console.WriteLine($"Cumulative error: [{string.Join(' ', cumulativeError.ToArray())}]");
+
+            return x;
         }
 
-        private static double Trapezoid(double time, double tmax, double T, Vector<double> start, Matrix<double> A,
+        private static Vector<double> Trapezoid(double tmax, double T, Vector<double> start, Matrix<double> A,
             Matrix<double> B, Func<double, double> r, bool verbose = false)
         {
+            Console.WriteLine($"\nTRAPEZOID(tmax: {tmax}, T: {T}, start: [{string.Join(' ', start.ToArray())}])");
             var U = MatrixBuilder.DenseIdentity(A.ColumnCount);
             var R = (U - A * 0.5 * T).Inverse() * (U + A * 0.5 * T);
 
             var S = (U - A * 0.5 * T).Inverse() * (0.5 * T) * B;
 
             var x = VectorBuilder.DenseOfVector(start);
-            var cumulativeError = 0.0;
+            var cumulativeError = VectorBuilder.Dense(A.RowCount);
             for (var t = T; t <= tmax; t += T)
             {
                 var pendulum = Generator.GeneratePendulum(start, t);
                 x = R * x + S * VectorBuilder.Dense(new[] {r(t - T) + r(t), r(t - T) + r(t)});
-                cumulativeError += (x - pendulum).L2Norm();
+
+                cumulativeError += Vector<double>.Abs(x - pendulum);
 
                 if (verbose)
                     Console.WriteLine(
-                        $"t: {t:F2} -- x: [{x[0]:g4} {x[1]:g4}] -- pendulum: [{pendulum[0]:g4} {pendulum[1]:g4}]");
+                        $"t: {t:F2} -- x: [{string.Join(' ', x.ToArray())}] -- pendulum: {string.Join(' ', pendulum.ToArray())}]");
             }
 
-            if (verbose)
-                Console.WriteLine($"\nCumulative error: {cumulativeError}");
-            return cumulativeError;
+            Console.WriteLine($"Cumulative error: [{string.Join(' ', cumulativeError.ToArray())}]");
+
+            return x;
         }
 
-        private static double RungeKutta(double time, double tmax, double T, Vector<double> start, Matrix<double> A,
+        private static Vector<double> RungeKutta(double tmax, double T, Vector<double> start, Matrix<double> A,
             Matrix<double> B, Func<double, double> r, bool verbose = false)
         {
+            Console.WriteLine($"\nRUNGE KUTTA(tmax: {tmax}, T: {T}, start: [{string.Join(' ', start.ToArray())}])");
             var x = VectorBuilder.DenseOfVector(start);
-            var cumulativeError = 0.0;
+            var cumulativeError = VectorBuilder.Dense(A.RowCount);
             for (var t = T; t <= tmax; t += T)
             {
                 var pendulum = Generator.GeneratePendulum(start, t);
                 var tk = t - T;
                 var m1 = A * x + B * VectorBuilder.Dense(new[] {r(tk), r(tk)});
-                var m2 = A * (x + (T / 2.0) * m1) + B * VectorBuilder.Dense(new[] { r(tk + (T / 2.0)), r(tk + (T / 2.0)) });
-                var m3 = A * (x + (T / 2.0) * m2) + B * VectorBuilder.Dense(new[] { r(tk + (T / 2.0)), r(tk + (T / 2.0)) });
-                var m4 = A * (x + T * m3) + B * VectorBuilder.Dense(new[] { r(tk + T), r(tk + T) });
+                var m2 = A * (x + (T / 2.0) * m1) +
+                         B * VectorBuilder.Dense(new[] {r(tk + (T / 2.0)), r(tk + (T / 2.0))});
+                var m3 = A * (x + (T / 2.0) * m2) +
+                         B * VectorBuilder.Dense(new[] {r(tk + (T / 2.0)), r(tk + (T / 2.0))});
+                var m4 = A * (x + T * m3) + B * VectorBuilder.Dense(new[] {r(tk + T), r(tk + T)});
 
                 x += (T / 6.0) * (m1 + 2 * m2 + 2 * m3 + m4);
 
-                cumulativeError += (x - pendulum).L2Norm();
+                cumulativeError += Vector<double>.Abs(x - pendulum);
                 if (verbose)
                     Console.WriteLine(
-                        $"t: {t:F2} -- x: [{x[0]:g4} {x[1]:g4}] -- pendulum: [{pendulum[0]:g4} {pendulum[1]:g4}]");
+                        $"t: {t:F2} -- x: [{string.Join(' ', x.ToArray())}] -- pendulum: {string.Join(' ', pendulum.ToArray())}]");
             }
 
-            if (verbose)
-                Console.WriteLine($"\nCumulative error: {cumulativeError}");
-            return cumulativeError;
+            Console.WriteLine($"Cumulative error: [{string.Join(' ', cumulativeError.ToArray())}]");
+            return x;
         }
     }
 }
